b83268795392a74a7fb21063b5aa19d0a9e04b41
uh-kay <konstantius.kevin@gmail.com>
2025-07-06 01:31:17 +0700

    fix embedding

--- main.go
+++ main.go
@@ -2,23 +2,30 @@ package main
 
 import (
 	"embed"
+	"io/fs"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"uhkay.com/templates"
 )
 
-//go:embed static
+//go:embed all:static
 var staticFiles embed.FS
 
 func main() {
 	mainComponent := templates.Main()
 
 	r := gin.Default()
+	r.SetTrustedProxies(nil)
 	r.GET("/", func(c *gin.Context) {
 		mainComponent.Render(c.Request.Context(), c.Writer)
 	})
-	r.StaticFS("/static", http.FS(staticFiles))
+
+	staticFS, err := fs.Sub(staticFiles, "static")
+	if err != nil {
+		panic(err)
+	}
+	r.StaticFS("/static", http.FS(staticFS))
 
 	r.Run(":8000")
 }
